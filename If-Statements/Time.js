8b18a41d5ae56e8ce31a47f8c38a8041c903727e
Jessica-vdz <jessicavdz02@gmail.com>
2024-10-17 14:21:03 +0200

    added some if statements

---
+++ If-Statements/Time.js
@@ -0,0 +1,13 @@
+﻿let huidigetijd = new Date(); // dit zoekt de tijd en datum van dit moment op.
+console.log(huidigetijd.getHours());
+
+
+if (huidigetijd.getHours() <9 ) 
+{
+    console.log("Je hoeft nog niet naar school!");
+}
+else if (huidigetijd.getHours() > 9){
+    console.log("SCHOOL TIJD!");
+}else if (huidigetijd.getHours() > 16){
+    console.log("VRIJE TIJD!!");
+}
---
+++ PlayerStats/PlayerStats.js
@@ -0,0 +1,25 @@
+﻿// 10 verschillende eigenschappen  -- 3 verschillende datatypes
+
+let name = "Bob";
+let age = 69;
+let superpower = "Superduper Lach";
+voorbeeld= true; // boolean
+let damage= 45;
+let weapon = "kniffie"
+let damageweapon= 89;
+let ennemy = "Trollen";
+let moeder= "Moeder Maria";
+let vader= "Vader Maria";
+
+
+
+console.log("Naam? " + name);
+console.log("Leeftijd? " + age);
+console.log("Vader? " + vader);
+console.log("Moeder? " + moeder);
+console.log("Superpower: " + superpower);
+console.log("Vijand? " + ennemy);
+console.log("Gezocht? " + voorbeeld);
+console.log("Wapen naar keuze: " + weapon);
+console.log("Hoe erg is het wapen beschadigd: " + damageweapon + "%.");
+console.log("damage? " + damage);
