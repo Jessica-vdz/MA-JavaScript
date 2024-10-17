0875ddb5d198e457211fa97e4b79b2c235fc53e0
Jessica-vdz <jessicavdz02@gmail.com>
2024-10-17 14:07:46 +0200

    added some more variables

--- PlayerStats.js
+++ PlayerStats.js
@@ -2,6 +2,24 @@
 
 let name = "Bob";
 let age = 69;
+let superpower = "Superduper Lach";
+voorbeeld= true; // boolean
+let damage= 45;
+let weapon = "kniffie"
+let damageweapon= 89;
+let ennemy = "Trollen";
+let moeder= "Moeder Maria";
+let vader= "Vader Maria";
 
-console.log("Naam: " + name);
-console.log("Leeftijd " + age);
+
+
+console.log("Naam? " + name);
+console.log("Leeftijd? " + age);
+console.log("Vader? " + vader);
+console.log("Moeder? " + moeder);
+console.log("Superpower: " + superpower);
+console.log("Vijand? " + ennemy);
+console.log("Gezocht? " + voorbeeld);
+console.log("Wapen naar keuze: " + weapon);
+console.log("Hoe erg is het wapen beschadigd: " + damageweapon + "%.");
+console.log("damage? " + damage);
