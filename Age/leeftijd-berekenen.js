5e340e83d6c672de7da3058937d3221906c9f40c
Jessica-vdz <jessicavdz02@gmail.com>
2024-11-07 13:51:51 +0100

    made the sentence better

--- Age/leeftijd-berekenen.js
+++ Age/leeftijd-berekenen.js
@@ -3,7 +3,7 @@
     let birth= 2003;
     let now= year - birth;
     
-    console.log("het is nu " + year + ". Ik ben geboren in " + birth + ". Wat betekent dat ik " + now + " jaar oud ben.");
+    console.log("het is nu " + year + ". Ik ben geboren in " + birth + ". Wat betekent dat ik nu" + now + " jaar oud ben.");
     return;
 }
 
